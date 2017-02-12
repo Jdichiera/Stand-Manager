983209d9318de317c72100d998331dc73756655e
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2017-02-12 08:32:36 -0600

    Create initial project files

---
+++ Stand-Manager/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * Author: Jeramy Dichiera
+ * Created on February 12, 2017, 8:30 AM
+ * File:   main.cpp
+ *
+ * Purpose: A simple console application that allows you to manage wedding
+ * cake stands.
+ * 
+ * Input:
+ * 
+ * Output:
+ * 
+ * Exceptions:
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Test";
+    return 0;
+}
+
